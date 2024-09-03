0069e80262506c5d235e22f9dea84a2cc6cb92c5
alvarito1999 <oj.alv99@gmail.com>
2024-09-03 18:44:12 -0300

    se mejoro el estilo del modal "gracias por su compra"

--- javascript/modal-quienes-somos.js
+++ javascript/modal-quienes-somos.js
@@ -3,6 +3,7 @@ const myModal = document.getElementById("myModal");
 const cerrar = document.querySelector(".close");
 
 myModal.style.width = "100vw";
+myModal.style.height = "100vh";
 myModal.style.position = "absolute";
 myModal.style.left = "0px";
 myModal.style.top = "0px";
