a58a0402b883f2503d7874c7a040d2b2c1b4303f
Alvaro Ojeda <=>
2024-10-03 20:29:19 -0300

    feat(paymentVista): vista de formulario de metodo de pago

--- src/js/vistas/payment.js
+++ src/js/vistas/payment.js
@@ -6,14 +6,8 @@ const payment = {
     removeBodyScroll();
     function setBackgroundOpacity() {
       const opacity = document.createElement("div");
-      opacity.style.zIndex = 3;
-      opacity.style.position = "absolute";
-      opacity.style.left = "0px";
-      opacity.style.bottom = "0px";
-      opacity.classList.add("opacity");
       opacity.id = "opacity-payment";
       document.body.appendChild(opacity);
-      document.body.style.overflow = "hidden";
     }
     setBackgroundOpacity();
     function setModal() {
@@ -42,15 +36,37 @@ const payment = {
   loadingSkeleton() {
     const loading = document.createElement("img");
     loading.src = "src/assets/img/loading.svg";
-    loading.style.width = "100px"
-    loading.style.height = "100px"
-    loading.style.zIndex = "5"
-    loading.id = "payment-loading-skeleton"
-    function removeLoading(){
-        document.getElementById("payment-loading-skeleton").remove()
+    loading.style.width = "100px";
+    loading.style.height = "100px";
+    loading.style.zIndex = "5";
+    loading.id = "payment-loading-skeleton";
+    function removeLoading() {
+      document.getElementById("payment-loading-skeleton").remove();
     }
-    setTimeout(removeLoading,1000)
+    setTimeout(removeLoading, 1000);
 
     document.getElementById("payment").appendChild(loading);
+  },
+  openChooseDeliveryMethod() {
+    function formChooseDeliveryMethod() {
+      const form = document.createElement("form");
+      form.id = "formChooseDeliveryMethod";
+      form.innerHTML = `
+      <h2>Selecciona tu método de entrega:</h2>
+        <label>
+            <input type="radio" name="opcion_envio" value="domicilio"> Enviar a domicilio
+        </label>
+        <label>
+            <input type="radio" name="opcion_envio" value="punto_entrega"> Retirar en punto de entrega
+        </label>
+        <label>
+            <input type="radio" name="opcion_envio" value="domicilio_vendedor"> Retirar en el domicilio del vendedor
+        </label>
+        <input type="submit" value="Continuar">
+        <button type="submit">Cancelar</button>`;
+      return form;
+    }
+    const payment = document.getElementById("payment");
+    payment.appendChild(formChooseDeliveryMethod());
   },
 };
