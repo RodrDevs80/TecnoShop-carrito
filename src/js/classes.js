d0ff4f9ce209fbfa7426c593b2b860a36c286480
RodrDevs80 <rodr26707@gmail.com>
2024-10-03 20:21:52 -0300

    grid-productos agregados-stylos-slider

--- src/js/classes.js
+++ src/js/classes.js
@@ -16,26 +16,8 @@ class Producto {
     imagen,
     categoria = "electrónica",
     cantidad = 1,
-    id= undefined
+    id = undefined
   ) {
-    // if (typeof nombre !== "string") {
-    //   throw new Error("El nombre debe ser un string");
-    // }
-    // if (typeof precio !== "number" || isNaN(precio)) {
-    //   throw new Error("El precio debe ser un número");
-    // }
-    // if (typeof descripcion !== "string") {
-    //   throw new Error("La descripción debe ser un string");
-    // }
-    // if (typeof categoria !== "string") {
-    //   throw new Error("La categoría debe ser un string");
-    // }
-    // if (typeof imagen !== "string") {
-    //   throw new Error("La imagen debe ser un string");
-    // }
-    // if (typeof cantidad !== "number" || isNaN(cantidad) || cantidad < 0) {
-    //   throw new Error("La cantidad debe ser un número entero positivo");
-    // }
     if (id == undefined) {
       this.id =
         (Math.random() * 1000).toFixed() * (Math.random() * 15).toFixed();
@@ -85,12 +67,10 @@ class Carrito {
     if (this.productos.length !== 0) {
       this.productos.forEach((producto) =>
         console.log(
-          `El ${producto.nombre} tiene un precio de $${
-            producto.precio
-          } por unidad\nCantidad: ${producto.cantidad} ${
-            producto.descripcion.length != 0
-              ? producto.descripcion
-              : "No hay descripción disponible"
+          `El ${producto.nombre} tiene un precio de $${producto.precio
+          } por unidad\nCantidad: ${producto.cantidad} ${producto.descripcion.length != 0
+            ? producto.descripcion
+            : "No hay descripción disponible"
           }`
         )
       );
